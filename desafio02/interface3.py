05808a1ddd0035e799c2afa99148adaf8e0f1722
Bruna Maria <bruna.mariap@outlook.com>
2023-11-25 23:05:45 -0300

    Correções na lógica de busca

--- desafio02/interface3.py
+++ desafio02/interface3.py
@@ -14,23 +14,24 @@ class Interface:
 
         self.predator_position = None
         self.prey_position = None
-        self.current_position_predator = self.predator_position
-        self.current_position_prey = self.prey_position
+        self.current_position_predator = None
+        self.current_position_prey = None
 
         self.current_state = "wall"
-        self.current_player = "predator" # Values: predator ou prey
+        self.current_player = "predator"  # Values: predator ou prey
 
         self.__pre_fill_matrix()
 
         self.max_num_square_predator_walk = 2
         self.max_num_square_prey_walk = 1
+        self.num_square_walk = 0
 
         # self.__update()
 
         self.canvas = Canvas(self.master, width=900, height=550)
         self.canvas.bind("<Button-1>", self.__set_positions)
         self.canvas.pack()
-        
+
         self.start_image_id = None
         self.goal_image_id = None
 
@@ -38,7 +39,8 @@ class Interface:
         self.background_image = self.background_image.resize((900, 550))
         self.background_photo = ImageTk.PhotoImage(self.background_image)
 
-        self.canvas.create_image(0, 0, anchor="nw", image=self.background_photo)
+        self.canvas.create_image(
+            0, 0, anchor="nw", image=self.background_photo)
 
         self.predator_image_path = "../imagens/pacman.png"
         self.start_image = Image.open(self.predator_image_path)
@@ -49,26 +51,29 @@ class Interface:
         self.goal_photo = ImageTk.PhotoImage(self.goal_image)
 
         self.state_button = None
-        
+
         self.open_set_predator = set()
         self.open_set_prey = set()
 
         self.visited_list_predator = set()
         self.visited_list_prey = set()
 
+        self.after_id = None  # ID do agendador do Tkinter
+
         self.count = 0
 
         self.__update()
 
-    def __pre_fill_matrix(self): # criação de matriz de objetos Square.
-        for x in range(self.grid_size): #loop para percorrer o labirinto
-            self.matrix.append([])# criação das linhas do labirinto.
+    def __pre_fill_matrix(self):  # criação de matriz de objetos Square.
+        for x in range(self.grid_size):  # loop para percorrer o labirinto
+            self.matrix.append([])  # criação das linhas do labirinto.
             for y in range(self.grid_size):
-                self.matrix[x].append(Square(x=x, y=y)) # criação de um novo quadrado  
+                # criação de um novo quadrado
+                self.matrix[x].append(Square(x=x, y=y))
 
     def draw_interface(self):
         self.master.mainloop()
-    
+
     def __get_square(self, row, col):
         if 0 <= row < self.grid_size and 0 <= col < self.grid_size:
             return self.matrix[row][col] if self.matrix[row][col] else None
@@ -88,13 +93,13 @@ class Interface:
         if self.current_state == "wall":
             current_square.state = "wall"
         elif self.current_state == "predator":
-            if self.predator_position is None:
+            if self.current_position_predator is None:
                 current_square.state = "predator"
-                self.predator_position = (row, col)
+                self.current_position_predator = (row, col)
         elif self.current_state == "prey":
-            if self.prey_position is None:
+            if self.current_position_prey is None:
                 current_square.state = "prey"
-                self.prey_position = (row, col)
+                self.current_position_prey = (row, col)
 
         self.__update()
 
@@ -131,7 +136,7 @@ class Interface:
 
         if self.state_button:
             self.state_button.destroy()
-        
+
         # Dicionário para armazenar as referências às imagens
         self.image_references = {}
 
@@ -174,7 +179,7 @@ class Interface:
                     }.get(state, "")
 
                     self.canvas.create_rectangle(
-                       ( y * self.square_size),
+                        (y * self.square_size),
                         (x * self.square_size),
                         (y + 1) * self.square_size,
                         (x + 1) * self.square_size,
@@ -193,7 +198,7 @@ class Interface:
             self.state_button.pack()
         elif self.current_state == "predator":
             self.state_button = Button(
-                self.master, text="Iniciar algoritmo", command=self.__find_path)
+                self.master, text="Iniciar algoritmo", command=self.update_interface)
             self.state_button.pack()
 
     def __heuristic(self, current, goal):
@@ -209,68 +214,63 @@ class Interface:
         else:
             return 10
         
-    def a_star_prey(self, start, goal):
-        pass
+    def reset_square_values(self, ):
+        for row in self.matrix:
+            for square in row:
+                square.g = float("inf")
+                square.h = 0
+                square.f = 0
 
-    def __find_path(self): #start, goal, open_list: set = set(), visited_list: set = set()):
-        # open_list = open_list
-        visited_list = set()
-        if self.current_player == "predator":
-            open_list = self.open_set_predator
-            start_square = self.__get_square(self.predator_position[0], self.predator_position[1])
-            goal_square = self.__get_square(self.prey_position[0], self.prey_position[1])
-        else:
-            open_list = self.open_set_prey
-            start_square = self.__get_square(self.prey_position[0], self.prey_position[1])
-            goal_square = self.__get_square(self.predator_position[0], self.predator_position[1])
-
-        start = (start_square.x, start_square.y)
-        goal = (goal_square.x, goal_square.y)
+    def __find_path(self, start: tuple, goal: tuple, open_list: set = set(), visited_list: set = set(), player=None):
+        start_square = self.__get_square(start[0], start[1])
+        goal_square = self.__get_square(goal[0], goal[1])
 
         start_square.g = 0
         start_square.h = self.__heuristic(start, goal)
         start_square.f = start_square.g + start_square.h
-        
+
         open_list.add(start_square)
-        count = 0
 
         # Falta alternar os valores pra cada player e alterar os valores e cada vez que eles se encontram
         while open_list:
             time.sleep(0.2)
 
-            if self.current_player == "predator":
-                current_square = self.__find_min_f(open_list)
-                current_position = (current_square.x, current_square.y)
-                self.predator_position = current_position
-                start = current_position
-            else:
-                current_square = self.__find_max_f(open_list)
-                current_position = (current_square.x, current_square.y)
-                self.prey_position = current_position
-                goal = current_position
+            current_square = self.__find_min_f(open_list)
+            current_position = (current_square.x, current_square.y)
 
-            print('posição atual:', current_position, self.current_player)
+            # print('posição atual:', current_position, self.current_player)
 
-            if self.current_position_predator == self.current_position_prey:
-                print("encontrou")
-                break
+            # if self.current_position_predator == self.current_position_prey:
+            #     print("encontrou")
+            #     self.master.after(200, self.__find_path)
 
-            if current_square == goal_square:
+            if current_position == goal:
                 path = self.__reconstruct_path(goal)
-                self.count += 1
-                if (self.current_player == "predator" and self.count == self.max_num_square_predator_walk):
-                    self.current_player = "prey"
-                elif (self.current_player == "prey" and self.count == self.max_num_square_prey_walk):
-                    self.current_player = "predator"
-                    # break
-                print(path)
-                self.master.after(200, self.__find_path)
+                self.num_square_walk += 1
+                
+                print('caminho a ser percorrido:', path)
+                if self.current_player == "predator":
+                    print("posição antiga", self.current_position_predator)
+                    print("posição a ser acessada", path[-2])
+                    self.current_position_predator = path[-2]
+
+                    self.reset_square_values()
+
+                    if self.current_position_predator != self.current_position_prey:
+                        # print("ainda n achou")
+                        self.current_position_predator = path[-3]
+                        # for i in range(self.max_num_square_predator_walk, 2, -1):
+                        #     pass
+                        print("posição a ser acessada 2", path[-3])
                 return True
 
             open_list.remove(current_square)
             visited_list.add(current_square)
 
             neighbors_squares = self.__find_neighbors(current_position)
+
+            # if self.current_player == "prey":
+            #     print('vizinhos da presa', neighbors_squares)
 
             for neighbor in neighbors_squares:
                 if neighbor in visited_list or neighbor.state == "wall":
@@ -283,34 +283,72 @@ class Interface:
                 if g_score < neighbor.g:
                     neighbor.parent = current_square
                     neighbor.g = g_score
-                    neighbor.h = self.__heuristic((neighbor.x, neighbor.y), goal)
+                    neighbor.h = self.__heuristic(
+                        (neighbor.x, neighbor.y), goal)
                     neighbor.f = neighbor.g + neighbor.h
                     open_list.add(neighbor)
 
-            # self.count += 1
+    def walk_prey(self, current_position: tuple, goal: tuple):
+        current_square = self.__get_square(current_position[0], current_position[1])
+        neighbors_squares = self.__find_neighbors(current_position)
 
-            # if (self.current_player == "predator" and self.count == self.max_num_square_predator_walk):
-            #     break
-            # elif (self.current_player == "prey" and self.count ==self.max_num_square_prey_walk):
-            #     break
+        for neighbor in neighbors_squares:
+            if neighbor.state == "wall":
+                continue
 
-            # self.current_player = "prey" if self.current_player == "predator" else "predator"
+            g_score = current_square.g + self.__calculate_g_score(
+                current_square, neighbor
+            )
 
+            if g_score < neighbor.g:
+                neighbor.parent = current_square
+                neighbor.g = g_score
+                neighbor.h = self.__heuristic((neighbor.x, neighbor.y), goal)
+                neighbor.f = neighbor.g + neighbor.h
 
-        # if self.current_player == "predator":
-        #     return self.__find_path(start, goal)
-        # else:
-        #     return self.__find_path(start, goal)
+        max_f_square = max(neighbors_squares, key=lambda square: square.f)
+        self.current_position_prey = (max_f_square.x, max_f_square.y)
+        self.num_square_walk += 1
+        print("nova posição presa", self.current_position_prey)
+        self.reset_square_values()
+        return max_f_square
 
-    # Predador usa
+    def update_interface(self):
+        """ Pegar as casas de pesquisa de acordo com o melhor caminho encontrado, acho que precisa percorrer toda vez que muda a posição """
+        while self.current_position_predator != self.current_position_prey:
+            if self.current_player == "predator" and self.num_square_walk >= 1:
+                print("entrou aqui")
+                self.current_player = "prey"
+                self.num_square_walk = 0
+            elif self.current_player == "prey" and self.num_square_walk >= self.max_num_square_prey_walk:
+                print("agora entrou aqui")
+                self.current_player = "predator"
+                self.num_square_walk = 0
+
+            print("player", self.current_player)
+            print('posição atual predador', self.current_position_predator)
+            print('posição atual presa', self.current_position_prey)
+            self.call_search_logic()
+            # self.master.after(200, self.call_search_logic)
+        else:
+            print("encontrou o caminho")
+            return True
+
+    def call_search_logic(self):
+        # and self.num_square_walk < self.max_num_square_predator_walk:
+        if self.current_player == "predator":
+            return self.__find_path(self.current_position_predator, self.current_position_prey, set(), set())
+        # and self.num_square_walk < self.max_num_square_prey_walk:
+        elif self.current_player == "prey":
+            # self.__find_path(self.current_position_prey, self.current_position_predator)
+            return self.walk_prey(self.current_position_prey, self.current_position_predator)
+
+        # self.__update()
+
     def __find_min_f(self, open_list):
         min_f_square = min(open_list, key=lambda square: square.f)
         return min_f_square
 
-    def __find_max_f(self, open_list):
-        max_f_square = max(open_list, key=lambda square: square.f)
-        return max_f_square
-    
     def __find_neighbors(self, current):
         x, y = current
         neighbors = []
@@ -343,17 +381,6 @@ class Interface:
                 self.current_position_prey[1]
             )
 
-    def __run_a_star(self):
-        self.__update()
-
-        path_found = self.__find_path(self.predator_position, self.prey_position)
-        if path_found:
-                print("Presa foi encontrada")
-        else:
-            self.__update()
-            print("Presa não encontrada")
-        self.player = "predator"
-
     def loop(self):
         self.update_positions()
 
@@ -367,20 +394,23 @@ class Interface:
     def grid_lines(self):
         for i in range(1, self.grid_size):
             x = i * self.square_size
-            self.canvas.create_line(x, 0, x, self.square_size * self.grid_size, fill="black", tags="grid")
+            self.canvas.create_line(
+                x, 0, x, self.square_size * self.grid_size, fill="black", tags="grid")
             y = i * self.square_size
-            self.canvas.create_line(0, y, self.square_size * self.grid_size, y, fill="black", tags="grid")
+            self.canvas.create_line(
+                0, y, self.square_size * self.grid_size, y, fill="black", tags="grid")
 
-    def __reconstruct_path(self, goal):
+    def __reconstruct_path(self, goal: tuple):
         path = []
-        current_square = self.__get_square(goal.x, goal.y)
+        current_square = self.__get_square(goal[0], goal[1])
 
         while current_square:
             path.append((current_square.x, current_square.y))
             current_square = current_square.parent
 
-        path.reverse()
+        # path.reverse()
         return path
+
 
 if __name__ == "__main__":
     interface = Interface()
